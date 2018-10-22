390e23ad6050c46eaba45df624445b832333ef8a
Mahdi Ebrahimi <azkia@iran.ir>
2018-10-22 08:39:41 +0330

    set the mysql setting

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('username');
+            $table->string('familyname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->smallInteger('permission');
+            $table->string('sign');
             $table->rememberToken();
             $table->timestamps();
         });
