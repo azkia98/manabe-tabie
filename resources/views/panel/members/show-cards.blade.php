12dd9d1d6e9b4f1dd9abc1fbb70c801d0ad0cf7a
Mahdi <mahdi@localhost.localdomain>
2018-11-04 18:28:06 +0330

    create first card :)

--- resources/views/panel/members/show-cards.blade.php
+++ resources/views/panel/members/show-cards.blade.php
@@ -12,11 +12,18 @@
             <div class="card">
                 <section class="name"><span>نام :</span> مهدی </section>
                 <section class="familyname"><span>نام خانوادگی :</span> ابراهیمی</section>
+                <section class="national-code"><span>کد ملی :</span> ۰۵۲۱۰۷۳۴۶۴</section>
+                <section class="fathername"><span>نام پدر :</span> حسین دهقانی</section>
+                <img class="picture" src="/images/Leila_Otadi_92.jpg" alt="">
+                <img class="signature" src="/images/signature.png" alt="">
+            </div>
+            <div class="card">
+                <section class="name"><span>نام :</span> مهدی </section>
                 <section class="familyname"><span>نام خانوادگی :</span> ابراهیمی</section>
+                <section class="national-code"><span>کد ملی :</span> ۰۵۲۱۰۷۳۴۶۴</section>
+                <section class="fathername"><span>نام پدر :</span> حسین دهقانی</section>
             </div>
         </div>
-    </div>
-    <div class="page">
     </div>
   </div>
 
