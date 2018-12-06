1dec68d00275fec259fc364cfc13059f1a1054c2
Mahdi Ebrahimi <azkia@iran.ir>
2018-12-06 13:27:02 +0330

    create validation for signature image file

--- app/Http/Controllers/Panel/OptionsController.php
+++ app/Http/Controllers/Panel/OptionsController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Panel;
 use App\Option;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class OptionsController extends Controller
 {
 
 
-    
+
     /**
      * Display a listing of the resource.
      *
@@ -19,9 +20,9 @@ class OptionsController extends Controller
     public function index()
     {
         auth()->loginUsingId(1);
-        $this->denied('options');   
+        $this->denied('options');
         $options = Option::all();
-        return view('panel.options.index',compact('options'));
+        return view('panel.options.index', compact('options'));
     }
 
     /**
@@ -36,18 +37,30 @@ class OptionsController extends Controller
         $option->value = $request->value;
         $option->update();
         alert()->success('تنظیم شما با موفقیت تنظیم گردید');
-        return ['msg'=>true];
+        return ['msg' => true];
     }
 
     /**
      * save signature picture
      */
-     public function updateSing(Request $request){
-         $path = $request->file('picture')->store('pictures', 'public');
-         $sigature_option = Option::GVWK('signature');
-         $sigature_option->value = $path;
-         $sigature_option->update();
-         alert()->success('امضاء شما با موفقیت تغییر پیدا کرد');
-         return redirect()->back();
-     }
+    public function updateSing(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'picture' => 'mimes:jpeg,bmp,png|max:200'
+        ]);
+
+        if ($validator->fails()) {
+            $messages = $validator->messages()->getMessages()['picture'][0];
+            alert()->error($messages);
+            return redirect()->back();
+            
+        }
+
+        $path = $request->file('picture')->store('pictures', 'public');
+        $sigature_option = Option::GVWK('signature');
+        $sigature_option->value = $path;
+        $sigature_option->update();
+        alert()->success('امضاء شما با موفقیت تغییر پیدا کرد');
+        return redirect()->back();
+    }
 }
